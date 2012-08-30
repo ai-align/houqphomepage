edb8b844d772d6117a728db89a3e10493888713c
Qingping Hou <dave2008713@gmail.com>
2012-08-30 12:25:20 +0800

    update timezone

--- pelican.conf.py
+++ pelican.conf.py
@@ -15,7 +15,7 @@ FEED_RSS = 'feeds/all.rss.xml'
 FEED_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
 
-TIMEZONE = '+404251-0740023'
+TIMEZONE = 'UTC'
 
 THEME = './scholar'
 CSS_FILE = 'style.css'
